8e3319bb90c9dffd7100c19e66f02737bd35c194
Kenny Clark <bigkenny15@yahoo.com>
2020-08-12 06:22:13 -0500

    Added comments to kc_logclear.py
    Added comments

--- shelf/kc_logclear.py
+++ shelf/kc_logclear.py
@@ -1,3 +1,6 @@
+# Simply copy paste this into a shelf tool script/code section.
+# This is meant to work alongside the kc_memorylog.py
+
 import hou
 hip= hou.getenv("HIP")
 path = "{}/log/log.txt".format(hip)
