b8df4e51ae1830160d81cf6a4bbc3d989209a5a9
Kenny Clark <bigkenny15@yahoo.com>
2020-08-12 05:55:42 -0500

    Add files via upload

--- kc_memorylog.py
+++ kc_memorylog.py
@@ -1,10 +1,12 @@
-node = hou.pwd()
-geo = node.geometry()
-
 # just copy paste this into a python node and place after your dopnet/importfield
-# this is a very quick and dirty script to log memory in a text file, just change 
+# this is a very quick and dirty script to log memory in a text file, just change
 # the path variable to where you need it to go. Default is $HIP/log/log.txt
 # works best in conjunction with the clear log shelf tool
+
+import hou
+
+node = hou.pwd()
+geo = node.geometry()
 
 memory = hou.hscript('memory')
 frame = hou.frame()
@@ -16,3 +18,9 @@ path = "$HIP/log/log.txt"
 file = f=open("%s" % path, "a+")
 file.write("{} {}\r\n".format(frame, memory))
 file.close()
+
+# import psutil
+# special note, if you're running Ubuntu 18.04+ psutil should be installed by
+# default, otherwise you can get it via pip. It's got some nice printouts and
+# you could log your system memory as well. I've noticed that my houdini memory
+# usuage doesn't always seem to line up with what system monitor is saying
